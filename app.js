236d20b5d75efcff71dab7d6cd2288df53de174b
SONG <SONG@SONGui-MacBook-Pro.local>
2020-08-26 11:02:29 +0900

    ADD dotenv

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //모듈 불러오기
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
@@ -7,6 +8,8 @@ const encrypt = require("mongoose-encryption");
 const mongooseEncryption = require("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 // 사용 설정
 app.set("view engine", "ejs");
@@ -23,9 +26,7 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisourlittlesecret.";
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
